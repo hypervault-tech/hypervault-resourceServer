e15ed9c467f8cab7140aa71152a4062c3beb6dca
Li Xi <xl404@cam.ac.uk>
2018-12-28 23:10:37 +0000

    Tested test route on resourceServer API

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const request = require("request-promise");
-
+const hypervaultAPIurl = "http://resourceserver.hypervault.tech:3000/api/";
+// const hypervaultAPIurl = "http://localhost:3000/api/";
 
 // define the home page route
 router.get('/', function (req, res) {
@@ -15,5 +16,7 @@ module.exports = router;
 
 
 async function testHandler(req, res) {
-
+  const resString = await request.get(hypervaultAPIurl+"tech.hypervault.User");
+  const users = JSON.parse(resString);
+  return res.send(users);
 }
