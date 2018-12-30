e4619eda8a56c8b02ebc0071ff020d879e8cbf8c
Li Xi <xl404@cam.ac.uk>
2018-12-30 15:59:42 +0000

    Added mkdirp module

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8081;
+const mkdirp = require('mkdirp');
+
+// first attempt to make ./resources/ directory to store all the files
+try {
+  mkdirp.sync("./resources");
+} catch (e) {
+  throw e;
+}
 
 const apiRouter = require("./routes/api");
 
 app.use("/api", apiRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Hypervault ResourceServer listening on port ${port}!`));
