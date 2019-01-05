2ce636c41dc5c117da84cfa11b48567e43318f8e
Li Xi <xl404@cam.ac.uk>
2019-01-05 17:55:58 +0000

    Changed config to a JS

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  version: "0.1.0",
+  // DO NOT ADD TRALIING SLASHES
+  
+  // path to save all resources
+  resourcesPath: "./resources",
+  // path to hold temporary uploads
+  tempResourcesPath: "./temp"
+}
+
+
+module.exports = config
