15434329c7a4c3fcfcf6036ab0a528aa432a30bc
Li Xi <xl404@cam.ac.uk>
2019-01-05 20:29:31 +0000

    Fixed endpoint which did not work upon more comprehensive test

--- routes/api.js
+++ routes/api.js
@@ -6,7 +6,12 @@ const fileUtil = require("../controllers/fileUtils");
 
 const multer  = require('multer');
 const upload = multer({ dest: apiConfig.tempResourcesPath });
+
+// to handle files
 const fs = require("fs");
+const util = require('util');
+const copyFile = util.promisify(fs.copyFile);
+const unlink = util.promisify(fs.unlink);
 
 router.get('/', async function (req, res) {
   pingResponse = await wrapper.pingNetwork();
@@ -36,41 +41,40 @@ async function uploadHandler(req, res) {
   // file is saved to ./temp
   const resourceId = req.params.resourceId;
   try {
+    // first check if a file has been uploaded
+    if (req.hasOwnProperty("file") == false)  {
+      return res.status(400).send("One and only one attached file is required. ");
+    }
+
     const resource = await wrapper.getResource(resourceId);
-    // first check if the resource exists
+    // next check if the resource exists
     if(resource == null) {
       return res.status(404).send("No resource is found with the given resourceId.");
     }
     // next check status of resource
     if(resource.status !== "PENDING_TRANSFER") {
       return res.status(400).send(`Resource status needs to be "PENDING_TRANSFER" for the resource to be accepted. `)
-    } else {
-      // proceed to check if owner exists
-      try {
-        const user = await wrapper.getResourceOwner(resourceId);
-      } catch(e) {
-        return res.status(400).send(`Something is wrong with the owner of the resource as the database gives an error when trying to read the user details.`);
-      }
-
-      // now check if the file has been uploaded
-      if (req.hasOwnProperty("file") === true) {
-        // next check hash of the file
-        const filehash = fileUtil.hashFile( req.file.path );
-        if(filehash !== resource.resourceId) {
-          return res.status(400).send("The filehash does not match the resourceId. ");
-        } else {
-          // now all checks are completed. Proceed to save the file and make it available. 
-          await fs.copyFile(req.file.path, `${apiConfig.resourcesPath}/${resource.resourceId}`);
-          await fs.unlink(req.file.path);
-          await wrapper.updateResource(resource.resourceId); 
-          // now return successful response 
-          res.status(201).send(resource.resourceId);
-        }
-      } else {
-        return res.status(400).send("One and only one attached file is required. ");
-      }
-      
+    } 
+    // proceed to check if owner exists
+    try {
+      const user = await wrapper.getResourceOwner(resourceId);
+    } catch(e) {
+      return res.status(400).send(`Something is wrong with the owner of the resource as the database gives an error when trying to read the user details.`);
     }
+
+    // Finally check hash of the file
+    const filehash = fileUtil.hashFile( req.file.path );
+    if(filehash !== resource.resourceId) {
+      return res.status(400).send("The filehash does not match the resourceId. ");
+    } 
+
+    // now all checks are completed. Proceed to save the file and make it available. 
+    await copyFile(req.file.path, `${apiConfig.resourcesPath}/${resource.resourceId}`);
+    await unlink(req.file.path);
+    await wrapper.updateResource(resource.resourceId); 
+    // now return successful response 
+    return res.status(201).send(resource.resourceId);
+
   }catch(e) {
     console.error(e);
     return res.status(500).send("Something went wrong. Please try again later.");
