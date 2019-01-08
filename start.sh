dd319a79ea459535e493008df5545a9b333eaf7a
Li Xi <xl404@cam.ac.uk>
2019-01-08 16:22:14 +0000

    Removed exit on error

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
-set -e 
 
 echo "Getting latest business network card from hypervault-id repository"
-set +e 
 rm resourceServer1.card
 composer card delete -c resourceServer1@hypervault
-set -e 
 wget https://github.com/lixiii/hypervault-id/raw/master/cards/resourceServer1.card
 composer card import -f resourceServer1.card
 rm resourceServer1.card
@@ -16,9 +13,7 @@ source ./start.sh
 
 cd ../
 echo "Restarting resource server"
-set +e 
 pm2 stop resourceServer 
-set -e 
 pm2 start server.js --name="resourceServer" 
 
 echo "Check server status by running: "
