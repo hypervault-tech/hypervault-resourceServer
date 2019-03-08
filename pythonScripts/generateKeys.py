3bfb90a5f79957aafe0c0c51ca037d075222a64a
Li Xi <xl404@cam.ac.uk>
2019-03-08 17:10:31 +0000

    Added umbral key generation script

---
+++ pythonScripts/generateKeys.py
@@ -0,0 +1,14 @@
+from umbral import config
+from umbral.curve import SECP256K1
+config.set_default_curve(SECP256K1)
+from umbral import keys, signing
+
+private_key = keys.UmbralPrivateKey.gen_key()
+public_key = private_key.get_pubkey()
+
+signing_key = keys.UmbralPrivateKey.gen_key()
+verifying_key = signing_key.get_pubkey()
+signer = signing.Signer(private_key=signing_key)
+
+print(private_key.to_bytes().hex())
+print(signing_key.to_bytes().hex())
